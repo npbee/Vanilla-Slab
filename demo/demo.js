9510910e43ec12179496c5bb19516d0a1dbd29e2
Nick Ball <nicholaspball@gmail.com>
2014-08-12 16:52:17 -0600

    fix last phrase

---
+++ demo/demo.js
@@ -0,0 +1,45 @@
+// Main initialization
+document.addEventListener('DOMContentLoaded', function() {
+
+  var vslab = VanillaSlab;
+  vslab.init({
+    maxWordsPerLine: 3,
+    minWordsPerLine: 2,
+    fontRatio: 0.86
+  });
+
+  // Debugging only
+
+  //for (var i = 0; i < vslab.targets.length; i++) {
+    //var content = document.createElement('div');
+    //var el = vslab.targets[i].element;
+    //var words = append_stuff(vslab.targets[i].words, "Words:: ");
+    //var lines = append_stuff(vslab.targets[i].lines, "Lines:: ");
+
+    //content.appendChild(words);
+    //content.appendChild(lines);
+
+    //el.parentNode.insertBefore(content, el.nextSibling);
+  //}
+
+  //function append_stuff(array, title) {
+    //var elm = document.createElement('p');
+    //elm.innerHTML = '<strong>' + title + '</strong>';
+
+    //array.forEach(function(item, index) {
+      //if (index === 0 && array.length === 1) {
+        //elm.innerHTML += '[ ' + item + ' ]';
+      //} else if (index === 0) {
+        //elm.innerHTML += '[ ' + item + ' / ';
+      //} else if (index === (array.length - 1)) {
+        //elm.innerHTML += item + ' ]';
+      //} else {
+        //elm.innerHTML += item + ' / ';
+      //}
+    //});
+
+    //return elm;
+  //}
+
+});
+
