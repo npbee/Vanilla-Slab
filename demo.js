04f45dba0843f39dcfcaa7d0a430fc08d1057a49
Nick Ball <nicholaspball@gmail.com>
2014-08-14 10:51:42 -0700

    updating demo page

--- demo.js
+++ demo.js
@@ -1,6 +1,13 @@
 // Main initialization
 document.onreadystatechange = function() {
   if (document.readyState === 'complete') {
+    var main_slab = new VanillaSlab;
+    main_slab.init({
+      selector: '.js-main-slab',
+      maxWordsPerLine: 1,
+      minWordsPerLine: 1
+    });
+    
     var vslab = new VanillaSlab;
     vslab.init({
       fontRatio: 1,
